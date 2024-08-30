810e9fbfbb090abf74b12e834823c6d6c4fd8482
Adam Brzozowski <RPY3UWV@posteitaliane.it>
2024-08-30 16:58:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/probability_calculator_app_migration/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.probability_calculator_app_migration
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.probability_calculator_app_migration.ui.theme.Probability_calculatorappmigrationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Probability_calculatorappmigrationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Probability_calculatorappmigrationTheme {
+        Greeting("Android")
+    }
+}
