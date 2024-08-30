8538fdaf4f61c5d19354f0c321caa2fe25efbf6e
Adam Brzozowski <RPY3UWV@posteitaliane.it>
2024-08-30 17:13:38 +0200

    Add first activity

---
+++ app/src/main/java/com/example/probability_calculator_app_migration/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.probability_calculator_app_migration;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+//        Intent i = new Intent(this, IntroActivity.class);
+//        startActivity(i);
+    }
+}
